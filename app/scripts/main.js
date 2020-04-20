72c6ff57b63bfb5a2f33bf918008cf8da95b57f9
unknown <samoylov82@gmail.com>
2020-04-20 19:34:31 +0300

    заканчиваю адаптив главной и наполнение

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,48 @@ $(document).ready(function() {
 
   $('.main__news-slider').slick({
     autoplay: false,
-    dots: false,
+    dots: true,
     arrows: true,
     slidesToShow: 1,
-    prevArrow: $('.prev'),
-    nextArrow: '<div class="next">'+
+    prevArrow: $('.news-prev'),
+    nextArrow: '<div class="news-next">'+
     '<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">'+
     '<path fill-rule="evenodd" clip-rule="evenodd" d="M12.414 7.75725C12.8045 7.36672 13.4377 7.36672 13.8282 7.75725L19.4851 13.4141C19.8756 13.8046 19.8756 14.4378 19.4851 14.8283L13.8282 20.4852C13.4377 20.8757 12.8045 20.8757 12.414 20.4852C12.0235 20.0946 12.0235 19.4615 12.414 19.071L17.3637 14.1212L12.414 9.17146C12.0235 8.78094 12.0235 8.14777 12.414 7.75725Z" fill="white"/>'+
     '<circle opacity="0.5" cx="14" cy="14" r="13" stroke="white" stroke-width="2"/>'+
     '</svg>'+
     '</div>'
+  })
+
+  // Brands slider 
+
+  $('.main__brands-slider').slick({
+    autoplay: false,
+    loop: true,
+    dots: true,
+    arrows: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    prevArrow: '<div class="brands-prev">'+
+    '<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">'+
+    '<path fill-rule="evenodd" clip-rule="evenodd" d="M16.586 7.75725C16.1955 7.36672 15.5623 7.36672 15.1718 7.75725L9.51495 13.4141C9.12442 13.8046 9.12442 14.4378 9.51495 14.8283L15.1718 20.4852C15.5623 20.8757 16.1955 20.8757 16.586 20.4852C16.9765 20.0946 16.9765 19.4615 16.586 19.071L11.6363 14.1212L16.586 9.17146C16.9765 8.78094 16.9765 8.14777 16.586 7.75725Z" fill="#C2BCBC"/>'+
+    '<circle r="13" transform="matrix(-1 0 0 1 14 14)" stroke="#C2BCBC" stroke-width="2"/>'+
+    '</svg>'+
+    '</div>',
+    nextArrow: '<div class="brands-next">'+
+    '<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">'+
+    '<path fill-rule="evenodd" clip-rule="evenodd" d="M12.414 7.75725C12.8045 7.36672 13.4377 7.36672 13.8282 7.75725L19.4851 13.4141C19.8756 13.8046 19.8756 14.4378 19.4851 14.8283L13.8282 20.4852C13.4377 20.8757 12.8045 20.8757 12.414 20.4852C12.0235 20.0946 12.0235 19.4615 12.414 19.071L17.3637 14.1212L12.414 9.17146C12.0235 8.78094 12.0235 8.14777 12.414 7.75725Z" fill="#C2BCBC"/>'+
+    '<circle opacity="0.5" cx="14" cy="14" r="13" stroke="#C2BCBC" stroke-width="2"/>'+
+    '</svg>'+
+    '</div>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {          
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }          
+      }
+    ]   
   })
 }) 
 
